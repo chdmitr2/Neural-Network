469528893dcd01dac4cd9cad9c1cdc1875db685c
DimaCh <chdmitr2@gmail.com>
2021-03-26 17:46:58 +0300

    Model of neural network

---
+++ Neural_Network/Model of Neural Network/Layer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neural_Network
+{
+    public class Layer
+    {
+        public List<Neuron> Neurons { get; }
+        public int NeuronCount => Neurons?.Count ?? 0;
+        public NeuronType Type;
+
+        public Layer(List<Neuron> neurons, NeuronType type = NeuronType.Normal)
+        {
+
+            Neurons = neurons;
+            Type = type;
+        }
+
+        public List<double> GetSignals()
+        {
+            var result = new List<double>();
+            foreach (var neuron in Neurons)
+            {
+                result.Add(neuron.Output);
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return Type.ToString();
+        }
+    }
+}
